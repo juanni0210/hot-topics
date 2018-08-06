6137ce7c9b9e714799b537f4c6807c19a724782e
Juan Ni <39057315+juanni0210@users.noreply.github.com>
2018-08-06 17:43:14 -0400

    Upload assignment folder

---
+++ js/index.js
@@ -0,0 +1,67 @@
+const mainContent = document.querySelector(".main-content");
+const navItems  = document.querySelectorAll(".navigation ul li a");
+let urlHome = "./partials/home.html";
+let urlPortfolio = "./partials/portfolio.html";
+
+
+function pageload() {
+     fetch(urlHome)
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (homeContent) {
+            mainContent.innerHTML = homeContent;
+        })
+        .catch(function (error) {
+            console.log(error.message);
+        });
+}
+
+
+
+function showContent (ev) {
+    let eventTargetObject = ev.target;
+    
+    eventTargetObject.setAttribute("id", "active-menu");
+
+    for (let i = 0; i < navItems.length; i++) {
+        if (navItems[i] !== eventTargetObject) {
+            navItems[i].removeAttribute("id", "active-menu");
+        }
+    }
+    
+    
+    let navLabel = eventTargetObject.textContent;
+    if (navLabel === "Home") {
+        
+        fetch(urlHome)
+            .then(function (response) {
+                return response.text();
+            })
+            .then(function (homeContent) {
+                mainContent.innerHTML = homeContent;
+            })
+            .catch(function (error) {
+                console.log(error.message);
+            });
+    } else if (navLabel === "Portfolio") {
+        
+        fetch(urlPortfolio)
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (portfolioContent) {
+            mainContent.innerHTML = portfolioContent;
+        })
+        .catch(function (error) {
+            console.log(error.message);
+        });
+    } 
+}
+
+
+
+
+for (let navItem of navItems) {
+    navItem.addEventListener("click", showContent);
+}
